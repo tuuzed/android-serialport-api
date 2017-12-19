76a4927a8da1b43f31b71a18d50e066467fa3ce8
TuuZed <liuyonghui.job@gmail.com>
2017-12-19 17:18:50 +0800

    v1.1.1

--- sample/src/main/java/android_serialport_api/sample/SerialPortPreferences.java
+++ sample/src/main/java/android_serialport_api/sample/SerialPortPreferences.java
@@ -26,40 +26,39 @@ import android_serialport_api.SerialPortFinder;
 
 public class SerialPortPreferences extends PreferenceActivity {
 
-	private Application mApplication;
-	private SerialPortFinder mSerialPortFinder;
+    private Application mApplication;
+    private SerialPortFinder mSerialPortFinder;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		mApplication = (Application) getApplication();
-		mSerialPortFinder = mApplication.mSerialPortFinder;
+        mApplication = (Application) getApplication();
+        mSerialPortFinder = mApplication.mSerialPortFinder;
 
-		addPreferencesFromResource(R.xml.serial_port_preferences);
+        addPreferencesFromResource(R.xml.serial_port_preferences);
 
-		// Devices
-		final ListPreference devices = (ListPreference)findPreference("DEVICE");
-        String[] entries = mSerialPortFinder.getAllDevices();
-        String[] entryValues = mSerialPortFinder.getAllDevicesPath();
-		devices.setEntries(entries);
-		devices.setEntryValues(entryValues);
-		devices.setSummary(devices.getValue());
-		devices.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-				preference.setSummary((String)newValue);
-				return true;
-			}
-		});
+        // Devices
+        final ListPreference devices = (ListPreference) findPreference("DEVICE");
+        String[] entriesAndValues = mSerialPortFinder.getAllDevicesPathNonRepeating();
+        devices.setEntries(entriesAndValues);
+        devices.setEntryValues(entriesAndValues);
+        devices.setSummary(devices.getValue());
+        devices.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                preference.setSummary((String) newValue);
+                return true;
+            }
+        });
 
-		// Baud rates
-		final ListPreference baudrates = (ListPreference)findPreference("BAUDRATE");
-		baudrates.setSummary(baudrates.getValue());
-		baudrates.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-				preference.setSummary((String)newValue);
-				return true;
-			}
-		});
-	}
+        // Baud rates
+        final ListPreference baudrates = (ListPreference) findPreference("BAUDRATE");
+        baudrates.setSummary(baudrates.getValue());
+        baudrates.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                preference.setSummary((String) newValue);
+                return true;
+            }
+        });
+    }
 }
